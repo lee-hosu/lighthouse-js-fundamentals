a33e4c60ce6e17e188aea54847fc2cb865cc91bd
Hosu <sogeumhosu@gmail.com>
2023-05-30 19:14:42 -0700

    arrays

---
+++ packing-list.js
@@ -0,0 +1,23 @@
+const packingList = [
+  "bowls",
+  "plates",
+  "pots",
+  "pans",
+  "eating utensils",
+  "glasses",
+  "cups",
+  "cooking utensils",
+];
+
+// for loops
+console.log("Kitchen stuff to pack:");
+for (let i = 0; i < packingList.length; i++) {
+  console.log(packingList[i]);
+}
+
+// while
+let i = 0;
+while (i < packingList.length) {
+  console.log(packingList[i]);
+  i++;
+}
